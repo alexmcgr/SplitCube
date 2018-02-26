a931916eb3f106463f4e851bdc9dc43a58e28d64
Alex McGregor <mcgregoralex3@gmail.com>
2018-02-26 14:56:27 -0800

    added a way to choose the construction plane that is being split on

--- SplitCube.py
+++ SplitCube.py
@@ -12,17 +12,20 @@ sketch = sketches.add(xyPlane)
 circles = sketch.sketchCurves.sketchCircles
 line = sketch.sketchCurves.sketchLines 
 
-
-
 #Equivalent to a main method in java
 def run(context):
     ui = None
     try:
+        
         dist = 5
         draw_rect(-2,-2,0,4,4,0, dist)
-        prof = sketch.profiles.item(0)       
-        test = extrude(prof, dist)
-        split(test, dist)
+        prof = sketch.profiles.item(0)  
+        ext = extrude(prof, dist)
+        # Set third parameter to > 0 to cut on the XZ plane, and < 0 to cut on YZ plane 
+        # If user input is ever implemented this would be a godd spot to add it
+        plane_value = -1
+        # Distance does not change anything in split yet
+        split(ext, dist, plane_value)
             
     except:
         if ui:
@@ -55,23 +58,17 @@ def extrude(prof, dist):
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
       
 #Splits a given body at a given distance
-def split(ext, distance):
+def split(ext, distance, plane_value):
     ui = None
     try:
-        '''
-        # Third parameter value controls what plane is the one being cut on, greater than 0 for XZ, less than 0 for YZ, and 0 for XY plane
-        # when this is implemented there will be a third parameter added        
-        #Also distance does not appear to be working
-        if test > 0:
-            plane = xZConstructionPlane
-        elif test < 0:
-            plane = yZConstructionPlane
+        if plane_value > 0:
+            plane = rootComp.xZConstructionPlane
         else:
-            plane = xYConstructionPlane
-        '''
+            plane = rootComp.yZConstructionPlane
+        
         body = ext.bodies.item(0)
         splitBodyFeats = rootComp.features.splitBodyFeatures            
-        splitBodyInput = splitBodyFeats.createInput(body, rootComp.yZConstructionPlane, True)
+        splitBodyInput = splitBodyFeats.createInput(body, plane, True)
         splitBodyFeats.add(splitBodyInput)
         
     except:
